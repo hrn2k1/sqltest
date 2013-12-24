4d0f0341178e79ed21c6d5cdbcd80a101f3fa809
hrn2k1 <Harun.Rashid@dev.dctieto.fi>
2013-12-24 16:14:22 +0600

    add new

--- app.js
+++ app.js
@@ -1,60 +1,38 @@
+
+/**
+ * Module dependencies.
+ */
+
 var express = require('express');
-var app = express.createServer();
-//var sql = require('node-sqlserver');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
 
-var conn_str = "Driver={SQL Server Native Client 11.0};" +
-               "Server=tcp:niqqqj7roh.database.windows.net,1433" +
-               "Database=TodoList6_db;Uid=TodoList6@niqqqj7roh;" +
-               "Pwd=Admin123;Encrypt=yes;Connection Timeout=30";
-var query = "SELECT id,text,complete FROM TodoItem";
-var port = process.env.port || 3331;
+var app = express();
 
-app.get('/', function (req, res) {
-    var taskSet = [];
-    /*sql.open(conn_str, function (err, conn) {
-        if (err) {
-            res.end("Error opening the connection!");
-            return;
-        }
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-        conn.queryRaw(query, function (err, results) {
-            if (err) {
-                res.end("Error running the task query");
-                return;
-            }
-            console.log("row count = " + results.rows.length + "\n");
-            for (var i = 0; i < results.rows.length; i++) {
-                res.write("ID: " + results.rows[i][0] +
-                          " text: " + results.rows[i][1] + 
-                          " Complete: " + results.rows[i][2] + "\n");
-            }
-            res.end('Done --\n');
-        });
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
-    }); // sql.open*/
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-app.listen(port);
-
-
-/*var sql = require('msnodesql');
-var conn_str = "Driver={SQL Server Native Client 10.0};Server=tcp:niqqqj7roh.database.windows.net,1433;Database=TodoList6_db;Uid=TodoList6@niqqqj7roh;Pwd=Admin123;Encrypt=yes;Connection Timeout=30;";
-
-var http = require('http')
-var port = process.env.port||3000;
-http.createServer(function (req, res) {
-    sql.query(conn_str, "SELECT * FROM TodoItem", function (err, results) {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.write("Got error :-( " + err);
-            res.end("");
-            return;
-        }
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        console.log(results);
-        res.write(results);
-        /*for (var i = 0; i < results.length; i++) {
-            res.write("ID: " + results[i].ID + " Column1: " + results[i].Column1 + " Column2: " + results[i].Column2);
-        }
-        res.end("; Done.");
-    });
-}).listen(port);*/
